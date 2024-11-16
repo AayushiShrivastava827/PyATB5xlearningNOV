6e8d5c56bd98ce91e93e6be138fd424f3b4f7785
ayushi Shrivastava <aayushishrivastava447@gmail.com>
2024-11-16 17:20:57 +0530

    9TH NOV 2024

---
+++ Nov/ex_09112024/task002.py
@@ -0,0 +1,10 @@
+#task
+#take 2 number from the input and print the Quotien and Reminder
+
+a = int(input("enter first number"))
+b = int(input("enter second number"))
+Quotient = a%b
+reminder = a/b
+
+print(Quotient)
+print(reminder)
